0c091a837579331fd979ab66a88c7e24ad4d5209
blvck2371 <lindoungapoutabdelraoufou@gmail.com>
2026-02-19 17:34:37 +0100

    teste de lancement de de mon serveur

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      configureServer() {
+        return () => {
+          console.log('🚀 [TheTipTop] Serveur frontend lancé avec succès sur http://localhost:5173')
+        }
+      }
+    }
+  ],
 })
