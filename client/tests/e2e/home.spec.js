f7a7a74cfd92e0c6c792965541dff35b01a3f178
Makata Jean Junior <makatajunior5@gmail.com>
2026-02-24 14:56:03 +0100

    import des  modules ES dans playwright

--- client/tests/e2e/home.spec.js
+++ client/tests/e2e/home.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test.describe('Home page', () => {
   test('displays main content', async ({ page }) => {
