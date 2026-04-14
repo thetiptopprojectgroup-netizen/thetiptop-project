96262c096f03e4b582b7e174e850a7694eccdc60
Acer <blvck2371>
2026-04-14 23:53:26 +0200

    refactor(client) : suppression de l’import React inutilisé dans entry-server.jsx

--- client/src/entry-server.jsx
+++ client/src/entry-server.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { HelmetProvider } from 'react-helmet-async';
